58f4610eacbfae480412295fffe1e852655c1e8a
debicky <debickiart@gmail.com>
2023-05-06 12:07:20 +0200

    add Header.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import * as serviceWorker from '.serviceWorker';
 import './index.css';
-import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import App from './App';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 const routing = (
-  <Router>
-    <React.StrictMode>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={App} />
-      </Switch>
-      <Footer />
-    </React.StrictMode>
-  </Router>
+    <Router>
+        <React.StrictMode>
+            <Header />
+            <Routes>
+                <Route path="/" element={<App />} />
+            </Routes>
+            {/*<Footer />*/}
+        </React.StrictMode>
+    </Router>
 )
+
+ReactDOM.createRoot(document.getElementById('root')).render(routing);
